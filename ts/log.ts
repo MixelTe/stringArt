7de7fc41bec7db886b38892d00e56913ffeee080
Mike <63655075+MixelTe@users.noreply.github.com>
2023-07-25 00:05:53 +0300

    init

---
+++ ts/log.ts
@@ -0,0 +1,29 @@
+import { Div, getDiv } from "./littleLib.js";
+
+const log = getDiv("log");
+console.log = Log;
+console.error = LogError;
+addEventListener("error", e => onError(e.message, `${e.filename}:${e.lineno}:${e.colno}`));
+addEventListener("unhandledrejection", e => onError("(in promise) " + e.reason));
+
+export function Log(...data: any[])
+{
+	log.appendChild(Div([], [], data.join(" ")));
+	log.scroll(0, log.scrollHeight);
+}
+
+export function LogError(...data: any[])
+{
+	log.appendChild(Div("error", [], data.join(" ")))
+	log.scroll(0, log.scrollHeight);
+}
+
+function onError(msg: string, path?: string)
+{
+	if (path)
+	{
+		const i = path?.lastIndexOf("/");
+		msg += "\n at " + path?.slice(i + 1)
+	}
+	LogError(msg);
+}
